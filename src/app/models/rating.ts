ebb148716fb76f3a0dc7e041e0491b72387f032a
chrisdev12 <christianm.bravop@gmail.com>
2020-05-01 21:13:52 -0500

    Formularios reactivos funcionando | Botón de nuevo gif random integrado | guardando datos en aso de votación en localStorage

---
+++ src/app/models/rating.ts
@@ -0,0 +1,5 @@
+export class Rating {
+  id:     number;
+  value:  number;
+  review: string;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ComicsService } from '../../services/comics.service';
 import { Comic } from 'src/app/models/comic';
-import { Form } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Rating } from 'src/app/models/rating';
 
 @Component({
   selector: 'app-home',
@@ -11,52 +12,101 @@ import { Form } from "@angular/forms";
 
 export class HomeComponent implements OnInit {
 
-  public comic: Comic
-  public loaded: Boolean = false;
-  public rating: string = 'Aún no has votado :('
+  public ratingForm: FormGroup;
+  private rating: Rating
+  public comic: Comic;
+  public loaded: Boolean;
+  public ratingDesc: string = 'Aún no has votado :(';
+  public id: number;
 
   constructor(
+    private formBuilder: FormBuilder,
     private _service: ComicsService,
-  ) { }
+  ) {
+    this.buildForm();
+    this.rating = new Rating();
+  }
 
   ngOnInit(): void {
-    this.getComic();
+    this.id = this.randomNumber(1, 2299);
+    this.getComic(this.id);
   }
 
-  getComic() {
-    this._service.getComics(1).subscribe((res: Comic) => {
+  getComic(id: number) {
+    this.loaded = false
+    this._service.getComics(id).subscribe((res: Comic) => {
       if (res) {
-
-        console.log(res);
         this.comic = res;
-        setTimeout(() => this.loaded = true, 1500)
+        setTimeout(() => this.loaded = true, 500)
       }
-    }, (err: any) => {
-        console.log(err)
-    });
-  }
-
-  rateComic() {
-    console.log('comic');
+    }, (err: any) => console.log(err));
   }
 
   getRating(event) {
     let value = parseInt(event.target.value)
     switch (value) {
-      case 1: this.rating = 'Quitalo de mi vista';
+      case 1: this.ratingDesc = 'Quitalo de mi vista';
         break;
-      case 2: this.rating = 'Meh';
+      case 2: this.ratingDesc = 'Meh';
         break;
-      case 3: this.rating = 'Tiene su no sé qué';
+      case 3: this.ratingDesc = 'Tiene su no sé qué';
         break;
-      case 4: this.rating = 'Está interesante';
+      case 4: this.ratingDesc = 'Está interesante';
         break;
-      case 5: this.rating = 'WOW';
+      case 5: this.ratingDesc = 'WOW';
         break;
-      case 6: this.rating = 'Gracias por tanto';
+      case 6: this.ratingDesc = 'Gracias por tanto';
         break;
-      default: this.rating = 'Aún no has votado :('
+      default: this.ratingDesc = 'Aún no has votado :('
         break;
     }
+  }
+
+  randomNumber(min: number,max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+
+  /**
+   * @BuildForm construir las validaciones para nuestros inputs
+   * @Getters Disparar los serrores en caso de que no se cumplan las validaciones
+   * @RatingFormInvalid verificar en el submit si los validores fueron cumplidos,
+   * de lo contrario mostrar en pantalla las condiciones que se necesitan.
+   */
+  buildForm() {
+    this.ratingForm = this.formBuilder.group({
+      value: ['', [Validators.required]],
+      review: ['', [Validators.required, Validators.minLength(15)]],
+    })
+  }
+
+  get reviewValidate() {
+    return this.ratingForm.get('review').invalid && (this.ratingForm.get('review').touched  || this.ratingForm.get('review').dirty);
+  }
+
+  get valueValidate() {
+    return this.ratingForm.get('value').invalid && (this.ratingForm.get('value').touched  || this.ratingForm.get('value').dirty);
+  }
+
+  rateComic() {
+    if (this.ratingForm.invalid) {
+      return Object.values(this.ratingForm.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
+    this.rating.id = this.id;
+    this.rating.value = this.ratingForm.get('value').value;
+    this.rating.review = this.ratingForm.get('review').value;
+    localStorage.setItem('ratings', JSON.stringify(this.rating));
+    this.ratingForm.reset();
+
+    //Traer un nuevo Comic random
+    this.id = this.randomNumber(1, 2299)
+    this.getComic(this.id);
+  }
+
+  nextComic() {
+    this.id = this.randomNumber(1, 2299)
+    this.getComic(this.id);
   }
 }
